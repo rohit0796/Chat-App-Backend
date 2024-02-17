7890df980e22cac49256c241abfbb68d825d8402
Rohit Kumar Sahu <91247476+rohit0796@users.noreply.github.com>
2024-02-17 13:41:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const mongoose = require('mongoose');
 const http = require('http');
 require('dotenv').config()
 const urlrouter = require('./routes');
-app.use(cors());
+
+app.use(cors(
+  origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+));
 
 app.use(express.json());
 
